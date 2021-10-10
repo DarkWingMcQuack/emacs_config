66bb6721b678d49a3376914b90193958d67d2c38
lukas <np-hardass@protonmail.com>
2021-10-10 15:11:16 +0200

    use better indent guide again

--- inits/indent-guide.el
+++ inits/indent-guide.el
@@ -1,19 +1,19 @@
-;; (use-package highlight-indent-guides
-;;   :hook
-;;   (prog-mode . highlight-indent-guides-mode)
-
-;;   :custom
-;;   (highlight-indent-guides-method 'character)
-;;   (highlight-indent-guides-character ?\┆)
-;;   (highlight-indent-guides-auto-enabled nil)
-
-;;   :config
-;;   (set-face-background 'highlight-indent-guides-odd-face "orange")
-;;   (set-face-background 'highlight-indent-guides-even-face "orange")
-;;   (set-face-foreground 'highlight-indent-guides-character-face "orange"))
-
-(use-package highlight-indentation
+(use-package highlight-indent-guides
   :hook
-  (prog-mode . highlight-indentation-mode)
+  (prog-mode . highlight-indent-guides-mode)
+
   :custom
-  (highlight-indentation-blank-lines t))
+  (highlight-indent-guides-method 'character)
+  (highlight-indent-guides-character ?\┆)
+  (highlight-indent-guides-auto-enabled nil)
+
+  :config
+  (set-face-background 'highlight-indent-guides-odd-face "orange")
+  (set-face-background 'highlight-indent-guides-even-face "orange")
+  (set-face-foreground 'highlight-indent-guides-character-face "orange"))
+
+;; (use-package highlight-indentation
+;;   :hook
+;;   (prog-mode . highlight-indentation-mode)
+;;   :custom
+;;   (highlight-indentation-blank-lines t))
