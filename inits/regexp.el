d90ddbd11b87e87bb34da5d0af20bc353d5f200b
Lukas <mail@lukas.contact>
2022-04-18 01:22:20 +0200

    fix regexps

--- inits/regexp.el
+++ inits/regexp.el
@@ -1,9 +1,13 @@
-(use-package visual-regexp
+(use-package visual-regexp)
+
+(use-package pcre2el)
+
+(use-package visual-regexp-steroids
   :commands (vr/replace vr/query-replace)
+  :after visual-regexp
+  :custom
+  (vr/engine 'pcre2el "Use PCRE regular expressions")
   :general
   (my-leader
 	:states 'normal
 	"r r" '(vr/replace :wk "regexp replace")))
-
-(use-package visual-regexp-steroids
-  :after visual-regexp)
