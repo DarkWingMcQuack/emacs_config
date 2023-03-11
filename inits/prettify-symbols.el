f41cd63a56b5b84ba662e1a3f2ffc01906635ac8
Lukas <mail@lukas.contact>
2023-03-11 17:41:46 +0100

    refactor pretify symbols

--- inits/prettify-symbols.el
+++ inits/prettify-symbols.el
@@ -1,6 +1,5 @@
-(global-prettify-symbols-mode 1)
-
 ;; pretiffy c++ range based for loops
+
 (defvar pretty-for-rgx
   (rx
    "for("
@@ -18,7 +17,6 @@
    (1+ word)))
 
 (defun pretty-for-add ()
-  ;; (setq font-lock-keywords nil)
   (font-lock-add-keywords
    nil
    `((,pretty-for-rgx 0 (prog1 nil
@@ -26,13 +24,4 @@
                            (match-beginning 1) (match-end 1)
                            ,(concat "\t" (list #x2208))))))))
 
-(add-hook 'c-mode-common-hook 'pretty-for-add)
-
-(add-hook
- 'python-mode-hook
- (lambda ()
-   (mapc (lambda (pair) (push pair prettify-symbols-alist))
-         '(("and"  .     "∧")
-           ("or"  .      "∨")
-           ("in" .       #x2208)
-           ("not in" .   #x2209)))))
+(add-hook 'c++-mode-common-hook 'pretty-for-add)
