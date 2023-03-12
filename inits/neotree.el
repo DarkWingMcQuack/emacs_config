8699f8dcb0dc53aade2ec412037a65c8a7afbccc
Lukas <mail@lukas.contact>
2023-03-12 16:51:44 +0100

    open neotree in git root

--- inits/neotree.el
+++ inits/neotree.el
@@ -1,13 +1,23 @@
 (use-package neotree
   :after evil
+  :config
+  (defun neotree-project-dir ()
+    "Open NeoTree using the git root."
+    (interactive)
+    (let ((project-dir (projectile-project-root))
+          (file-name (buffer-file-name)))
+      (neotree-toggle)
+      (if project-dir
+          (if (neo-global--window-exists-p)
+              (progn
+                (neotree-dir project-dir)
+                (neotree-find file-name)))
+        (message "Could not find git project root."))))
   :bind
-  ([f8] . neotree-toggle)
+  ([f8] . neotree-project-dir)
 
   :custom
-  (neo-theme
-   (if (display-graphic-p)
-       'icons
-     'arrow))
+  (neo-theme (if (display-graphic-p) 'icons 'arrow))
 
   :general
   (general-define-key
