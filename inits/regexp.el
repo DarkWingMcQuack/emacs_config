dfb8722cb453d30cb6d254aa8a637f3603719e64
Lukas <mail@lukas.contact>
2023-03-12 16:51:36 +0100

    use pcre regexps

--- inits/regexp.el
+++ inits/regexp.el
@@ -1,10 +1,11 @@
 (use-package visual-regexp)
-
 (use-package pcre2el)
 
 (use-package visual-regexp-steroids
   :commands (vr/replace vr/query-replace)
   :after visual-regexp
+  :custom
+  (vr/engine 'pcre2el "Use PCRE regular expressions")
   :general
   (my-leader
     :states 'normal
