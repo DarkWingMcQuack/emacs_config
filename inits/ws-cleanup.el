ca5f095d3d437697e7c79dd984eb31edc79892cd
Lukas <mail@lukas.contact>
2023-03-12 17:02:39 +0100

    added some comments

--- inits/ws-cleanup.el
+++ inits/ws-cleanup.el
@@ -6,18 +6,20 @@
 (use-package whitespace-cleanup-mode
   :custom
   (tab-width 4)
+  ;; show trailing whitespace
   (show-trailing-whitespace t)
 
   :config
   (add-hook 'before-save-hook (lambda () (untabify-except-makefiles)))
+  ;; enable ws-cleanup-mode globally
   (global-whitespace-cleanup-mode)
+  ;; don't use tabs to indent
   (setq-default indent-tabs-mode nil)
 
+  ;; don't show the trailing whitespace in special buffers like the minibuffer
   (dolist (hook '(special-mode-hook
                   term-mode-hook
                   comint-mode-hook
                   compilation-mode-hook
                   minibuffer-setup-hook))
-    (add-hook hook (lambda () (setq show-trailing-whitespace nil))))
-
-  )
+    (add-hook hook (lambda () (setq show-trailing-whitespace nil)))))
