d9d10abf645b40838241ec2fc6af09f62d0c0142
Lukas <mail@lukas.contact>
2023-03-12 17:00:52 +0100

    dont show trailing whitespaces in special buffers like minibuffer

--- inits/ws-cleanup.el
+++ inits/ws-cleanup.el
@@ -4,10 +4,20 @@
     (untabify (point-min) (point-max))))
 
 (use-package whitespace-cleanup-mode
+  :custom
+  (tab-width 4)
+  (show-trailing-whitespace t)
+
   :config
   (add-hook 'before-save-hook (lambda () (untabify-except-makefiles)))
   (global-whitespace-cleanup-mode)
   (setq-default indent-tabs-mode nil)
-  :custom
-  (tab-width 4)
-  (show-trailing-whitespace t))
+
+  (dolist (hook '(special-mode-hook
+                  term-mode-hook
+                  comint-mode-hook
+                  compilation-mode-hook
+                  minibuffer-setup-hook))
+    (add-hook hook (lambda () (setq show-trailing-whitespace nil))))
+
+  )
