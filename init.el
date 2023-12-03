82abb34c989bfc47e20ce2bb6f4847743a8fb1fc
Lukas <mail@lukas.contact>
2023-12-03 15:54:49 +0100

    drop OPAM

--- init.el
+++ init.el
@@ -70,6 +70,3 @@
 (load-user-file "inits/dimmer.el")
 (load-user-file "inits/mode-icons.el")
 (load-user-file "inits/multi-cursor.el")
-;; ## added by OPAM user-setup for emacs / base ## 56ab50dc8996d2bb95e7856a6eddb17b ## you can edit, but keep this line
-(require 'opam-user-setup "~/.emacs.d/opam-user-setup.el")
-;; ## end of OPAM user-setup addition for emacs / base ## keep this line
