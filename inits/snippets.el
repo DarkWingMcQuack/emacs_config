8cc7df40898ad0a34b23cddb7537e571d00ef216
Lukas <mail@lukas.contact>
2024-08-28 15:34:44 +0200

    add yassnippets

--- inits/snippets.el
+++ inits/snippets.el
@@ -1,3 +1,14 @@
 (use-package yasnippet
+  :hook
+  (prog-mode . yas-minor-mode)
+
+  :general
+  (my-leader 'prog-mode-map
+    :states 'normal
+    "s n" '(yas-new-snippet :wk "new snipped")
+    "s g" '(yas-visit-snippet-file :wk "goto snipped file")
+    "s i" '(yas-insert-snippet :wk "insert snipped from list"))
+  
   :config
-  (yas-global-mode))   
+  (yas-reload-all)
+  (setq yas-snippet-dirs '("~/.emacs.d/snippets")))
