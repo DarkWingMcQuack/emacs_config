3480bd41dcc28a172905aa691770829e57db5de6
Lukas <mail@lukas.contact>
2024-08-30 02:13:45 +0200

    disable fringes

--- inits/flycheck.el
+++ inits/flycheck.el
@@ -1,6 +1,7 @@
 (use-package flycheck
   :custom
   (flycheck-display-errors-delay 1)
+  (flycheck-indication-mode nil)
   (flycheck-check-syntax-automatically '(mode-enabled save new-line idle-change))
   :config
   (global-flycheck-mode))
