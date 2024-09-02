c7be011a1c2ab5cf19c6d4a0b64b43ca7bdf20d5
Lukas <mail@lukas.contact>
2024-09-02 14:46:58 +0200

    much better fuzzy matching

--- inits/orderless.el
+++ inits/orderless.el
@@ -1,4 +1,4 @@
 (use-package orderless
   :custom
-  (completion-styles '(orderless))
+  (completion-styles '(orderless flex))
   (completion-category-overrides '((file (styles basic partial-completion)))))
