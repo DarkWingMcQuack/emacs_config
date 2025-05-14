68c7e6d1639212aaeb83520d1a2fb09b76ecae93
Lukas <mail@lukas.contact>
2025-05-14 19:49:18 +0200

    dont use eldoc-box

--- inits/elisp.el
+++ inits/elisp.el
@@ -1,12 +1,7 @@
-(use-package eldoc
+(use-package emacs-lisp-mode
   :ensure nil
-  :after cape yasnippet-capf
+  :defer t
   :preface
-  ;; https://github.com/progfolio/elpaca/issues/236#issuecomment-1879838229
-  (unload-feature 'eldoc t)
-  (setq custom-delayed-init-variables '())
-  (defvar global-eldoc-mode nil)
-
   (defun my/elisp-format-buffer ()
     "Indent, untabify, and strip trailing whitespace in the whole buffer."
     (interactive)
@@ -14,6 +9,13 @@
       (indent-region (point-min) (point-max))
       (untabify    (point-min) (point-max))
       (delete-trailing-whitespace)))
+
+  (defun my/emacs-lisp-capf ()
+    (setq-local completion-at-point-functions
+                (list (cape-capf-super
+                       'elisp-completion-at-point
+                       'yasnippet-capf
+                       'cape-file))))
 
   (defun my/greek-lambda ()
     (font-lock-add-keywords nil `(("\\<lambda\\>"
@@ -23,23 +25,23 @@
                                                       ,(make-char 'greek-iso8859-7 107))
                                       nil))))))
 
-  (defun my/emacs-lisp-capf ()
-    (setq-local completion-at-point-functions
-                (list (cape-capf-super
-                       'elisp-completion-at-point
-                       'yasnippet-capf
-                       'cape-file))))
+  :hook
+  (emacs-lisp-mode . my/emacs-lisp-capf)
+  (emacs-lisp-mode . my/greek-lambda)
+  (emacs-lisp-mode . turn-on-eldoc-mode)
+
   :general
   (my-leader
     :keymaps 'emacs-lisp-mode-map
     :states 'normal
     "TAB" '(my/elisp-format-buffer :wk "format elisp buffer")
-    "b x" '(eval-buffer :wk "execute elisp buffer"))
+    "b x" '(eval-buffer :wk "execute elisp buffer")))
 
-  :config
-  (global-eldoc-mode)
-
-  :hook
-  (emacs-lisp-mode . my/emacs-lisp-capf)
-  (emacs-lisp-mode . turn-on-eldoc-mode)
-  (emacs-lisp-mode . my/greek-lambda))
+(use-package eldoc
+  :preface
+  ;; we need this because of some obscure elpaca bug
+  (unload-feature 'eldoc t)
+  (setq custom-delayed-init-variables '())
+  (defvar global-eldoc-mode nil)
+  :defer t
+  :ensure nil)
