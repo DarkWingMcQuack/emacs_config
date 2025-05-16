f19e373da9547347efe85a24269707c451a32eb9
Lukas <mail@lukas.contact>
2025-05-16 18:43:11 +0200

    formating

--- inits/dashboard.el
+++ inits/dashboard.el
@@ -10,11 +10,10 @@
   (dashboard-image-banner-max-height 300)
   (dashboard-items '((recents  . 5)
                      (projects . 5)
-		     (bookmarks . 5)))
+                     (bookmarks . 5)))
   (dashboard-display-icons-p t)
   (dashboard-icon-type 'nerd-icons)
   (dashboard-set-heading-icons t)
   (dashboard-set-file-icons t)
 
-  (dashboard-footer-messages
-  '("Free as free speech, free as free Beer")))
+  (dashboard-footer-messages '("Free as free speech, free as free Beer")))
