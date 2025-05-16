e13875cdc97976c0f29bb87d1d84e47c4abcaf70
Lukas <mail@lukas.contact>
2025-05-16 18:40:44 +0200

    lazy load crux

--- inits/crux.el
+++ inits/crux.el
@@ -1,8 +1,11 @@
 (use-package crux
+  :commands (crux-transpose-windows
+             crux-switch-to-previous-buffer
+             crux-rename-file-and-buffer
+             crux-create-scratch-buffer)
   :general
   (my-leader
     :keymaps 'override
     "w s" '(crux-transpose-windows :wk "switch windows")
     "b b" '(crux-switch-to-previous-buffer :wk "switch to last buffer")
     "f r" '(crux-rename-file-and-buffer :wk "rename file")))
-
