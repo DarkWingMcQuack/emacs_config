fcee436d86fc37f349168dd8d31c28bf6af4472c
Lukas <mail@lukas.contact>
2025-05-16 19:17:36 +0200

    show current line and column

--- inits/modeline.el
+++ inits/modeline.el
@@ -1,9 +1,13 @@
 (use-package doom-modeline
   :hook (elpaca-after-init . doom-modeline-mode)
+  :hook (doom-modeline-mode  . column-number-mode)
+
   :custom
-  (doom-modeline-modal-modern-icon t)
   (doom-modeline-height 25)
+  (doom-modeline-modal-modern-icon t)
   (doom-modeline-major-mode-color-icon t)
   (doom-modeline-buffer-state-icon t)
   (doom-modeline-percent-position '(-3 "%p"))
-  (doom-modeline-position-column-line-format '("%l:%c")))
+  (doom-modeline-position-line-format   '("L%l"))
+  (doom-modeline-position-column-format '("C%c"))
+  (doom-modeline-position-column-line-format '("L%l C%c")))
