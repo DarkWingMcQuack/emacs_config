e25fb9f6dd2c1b38958a6d53948209c5a6c86221
Lukas <mail@lukas.contact>
2025-05-16 19:23:40 +0200

    tweak modeline

--- inits/modeline.el
+++ inits/modeline.el
@@ -8,7 +8,17 @@
   (doom-modeline-modal-modern-icon t)
   (doom-modeline-major-mode-color-icon t)
   (doom-modeline-buffer-state-icon t)
+
   (doom-modeline-percent-position '(-3 "%p"))
+
   (doom-modeline-position-line-format   '("L%l"))
   (doom-modeline-position-column-format '("C%c"))
-  (doom-modeline-position-column-line-format '("L%l C%c")))
+  (doom-modeline-position-column-line-format '("L%l C%c"))
+
+  (doom-modeline-checker-simple-format nil)
+  (doom-modeline-lsp t)
+  (doom-modeline-buffer-encoding nil)
+  (doom-modeline-region-info t)
+  (doom-modeline-project-detection 'project)
+  (doom-modeline-project-icon t)
+  (doom-modeline-workspace-name t))
