d3f7c86af5468c039a7d752fa077c0caece507c2
Lukas <mail@lukas.contact>
2025-06-24 18:12:19 +0200

    added elm and php

---
+++ inits/elm.el
@@ -0,0 +1,17 @@
+(use-package elm-mode
+  :hook
+  (elm-mode . eglot-ensure)
+  (elm-mode . prettify-symbols-mode)
+  (elm-mode . (lambda ()
+                (mapc (lambda (pair) (push pair prettify-symbols-alist))
+                      '(("->" . #x2192)
+                        ("<-" . #x2190)
+                        ("\\" . #x03BB)
+                        ("=>" . #x21D2)
+                        ("==" . #x2261)
+                        ("/=" . #x2260)))))
+  :general
+  (my-leader
+    :states 'normal
+    :keymaps '(elm-mode-map eglot-mode-map)
+    "TAB" 'elm-format-buffer))
