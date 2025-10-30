60d4a29f2a8e0eef19eaf6ac74af2d15610daa11
Lukas <mail@lukas.contact>
2025-10-30 15:03:22 +0100

    added languagetools as correction stuff. currently cloud based, should switch to local

--- inits/writing.el
+++ inits/writing.el
@@ -18,11 +18,31 @@
     :states 'normal
     "l w" '(mw-thesaurus-lookup-at-point  :wk "lookup word")))
 
+
 (use-package academic-phrases
-  :ensure(academic-phrases :host github :repo "nashamri/academic-phrases")
+  :ensure (academic-phrases :host github :repo "nashamri/academic-phrases")
+
   :commands (academic-phrases academic-phrases-by-section)
 
   :general
   (my-leader
     :states 'normal
-    "l p" '(academic-phrases :wk "lookup word")))
+    "l p" '(academic-phrases :wk "academic phrases")))
+
+(use-package flymake-languagetool
+  :hook
+  (text-mode       . flymake-languagetool-load)
+  (latex-mode      . flymake-languagetool-load)
+  (LaTeX-mode      . flymake-languagetool-load)
+  (org-mode        . flymake-languagetool-load)
+  (markdown-mode   . flymake-languagetool-load)
+
+  :custom
+
+  (flymake-languagetool-server-jar nil)
+  (flymake-languagetool-url "https://api.languagetool.org")
+
+  :general
+  (general-define-key
+   :states 'normal
+   "c w" '(flymake-languagetool-correct-at-point :wk "correct word")))
