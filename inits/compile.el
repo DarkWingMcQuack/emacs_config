b240cc769bd9e3abe5291c3d52a3deb18cd45199
Lukas <mail@lukas.contact>
2026-05-11 00:01:31 +0200

    Guess project compile commands

--- inits/compile.el
+++ inits/compile.el
@@ -1,12 +1,90 @@
 (use-package compile
   :ensure nil
   :preface
+  (defun my/project-root-directory ()
+    "Return the current project root or `default-directory'."
+    (or (when-let ((project (project-current nil default-directory)))
+          (project-root project))
+        default-directory))
+
+  (defvar my/project-compile-root-files
+    '("CMakeLists.txt"
+      "build.sbt"
+      "build.sc"
+      "pubspec.yaml"
+      "Cargo.toml"
+      "go.mod"
+      "pom.xml"
+      "gradlew"
+      "build.gradle"
+      "build.gradle.kts"
+      "package.json"
+      "flake.nix"
+      "Makefile"
+      "makefile")
+    "Files that identify a useful root for project compilation.")
+
+  (defun my/project-compile-root-directory ()
+    "Return the nearest build-root directory or the current project root."
+    (or (catch 'root
+          (dolist (file my/project-compile-root-files)
+            (when-let ((root (locate-dominating-file default-directory file)))
+              (throw 'root root))))
+        (my/project-root-directory)))
+
+  (defun my/project-file-exists-p (file)
+    "Return non-nil when FILE exists in the current compile root."
+    (file-exists-p (expand-file-name file (my/project-compile-root-directory))))
+
+  (defun my/project-file-contains-p (file regexp)
+    "Return non-nil when FILE in the current compile root matches REGEXP."
+    (let ((path (expand-file-name file (my/project-compile-root-directory))))
+      (when (file-readable-p path)
+        (with-temp-buffer
+          (insert-file-contents path)
+          (re-search-forward regexp nil t)))))
+
+  (defun my/project-compile-command ()
+    "Guess a useful compile command for the current project."
+    (cond
+     ((my/project-file-exists-p "CMakeLists.txt")
+      "cmake -S . -B build && cmake --build build")
+     ((my/project-file-exists-p "build.sbt")
+      "sbt compile")
+     ((my/project-file-exists-p "build.sc")
+      "mill __.compile")
+     ((my/project-file-contains-p "pubspec.yaml" "^\\s-*flutter:")
+      "flutter build linux")
+     ((and (my/project-file-exists-p "pubspec.yaml")
+           (my/project-file-exists-p "bin/main.dart"))
+      "dart compile exe bin/main.dart")
+     ((my/project-file-exists-p "pubspec.yaml")
+      "dart test")
+     ((my/project-file-exists-p "Cargo.toml")
+      "cargo build")
+     ((my/project-file-exists-p "go.mod")
+      "go test ./...")
+     ((my/project-file-exists-p "pom.xml")
+      "mvn test")
+     ((my/project-file-exists-p "gradlew")
+      "./gradlew build")
+     ((or (my/project-file-exists-p "build.gradle")
+          (my/project-file-exists-p "build.gradle.kts"))
+      "gradle build")
+     ((my/project-file-exists-p "package.json")
+      "npm run build")
+     ((my/project-file-exists-p "flake.nix")
+      "nix flake check")
+     ((or (my/project-file-exists-p "Makefile")
+          (my/project-file-exists-p "makefile"))
+      "make")
+     (t compile-command)))
+
   (defun my/project-compile ()
-    "Run `compile' from the current project root when available."
+    "Run `compile' from the project root with a guessed command."
     (interactive)
-    (let ((default-directory (or (when-let ((project (project-current)))
-                                   (project-root project))
-                                 default-directory)))
+    (let ((default-directory (my/project-compile-root-directory))
+          (compile-command (my/project-compile-command)))
       (call-interactively #'compile)))
 
   (defun my/recompile ()
